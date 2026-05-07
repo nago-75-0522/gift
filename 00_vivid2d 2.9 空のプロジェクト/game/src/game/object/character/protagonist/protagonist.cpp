69996cc280404e6d37a77a4d5e1c0b24dfd39feb
oibc37191 <oibc37191@ojs.ac.jp>
2026-05-07 11:05:24 +0900

    Update protagonist.cpp

--- 00_vivid2d 2.9 空のプロジェクト/game/src/game/object/character/protagonist/protagonist.cpp
+++ 00_vivid2d 2.9 空のプロジェクト/game/src/game/object/character/protagonist/protagonist.cpp
@@ -1,1 +1,25 @@
 /* 主人公 */
+<<<<<<< Updated upstream
+=======
+#include"protagonist.h"
+
+CProtagonist::CProtagonist(void)
+{
+}
+
+void CProtagonist::Initialize(void)
+{
+}
+
+void CProtagonist::Update(void)
+{
+}
+
+void CProtagonist::Draw(void)
+{
+}
+
+void CProtagonist::Finalize(void)
+{
+}
+>>>>>>> Stashed changes
